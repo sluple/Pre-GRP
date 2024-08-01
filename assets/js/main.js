863f28ff7802a074b379bf2d8d1c514469d12d42
sluple <107330357+sluple@users.noreply.github.com>
2024-08-01 16:05:31 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-]/*==================== SHOW MENU ====================*/
+/*==================== SHOW MENU ====================*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
